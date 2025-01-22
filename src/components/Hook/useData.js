80ff04e99434c9eb55dbc16add978471648578e3
rok <rok@gmail.com>
2025-01-22 10:19:38 +0900

    로딩변수 추가

--- src/components/Hook/useData.js
+++ src/components/Hook/useData.js
@@ -4,13 +4,21 @@ import apiClient from "../../utils/api-client";
 const useData = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false); //로딩중인지
   useEffect(() => {
+    setIsLoading(true); //로딩 시작
     apiClient
       .get(url)
-      .then((res) => setData(res.data))
-      .catch((err) => setError(err.message));
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false); //로딩 끝
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false); //로딩 끝
+      });
   }, []);
-  return { data, error };
+  return { data, error, isLoading };
 };
 
 export default useData;
