9e51cc2ce591dad0045c5cf7a58da97ff35e439d
rok <rok@gmail.com>
2025-01-23 15:04:22 +0900

    토큰을 axios 헤더에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
 
+//만약 토큰이 있다면 axios 설정에 추가된다
+setAuthToken(localStorage.getItem("token"));
+
 function App() {
   const [user, setUser] = useState(null);
   const [cart, setCart] = useState([]); //장바구니
+  const addToCart = (product, quantity) => {
+    const updatedCart = [...cart]; //장바구니 복사
+    const productIndex = updatedCart.findIndex(
+      (item) => item.product._id === product._id
+    );
+    if (productIndex === -1) {
+      updatedCart.push({ product, quantity });
+    } else {
+      updatedCart[productIndex].quantity += quantity;
+    }
+    setCart([updatedCart]);
+  };
   useEffect(() => {
     try {
       const jwt = localStorage.getItem("token");
@@ -22,7 +37,7 @@ function App() {
     <div className="app">
       <Navbar user={user} cartCount={cart.length} />
       <main>
-        <Routing />
+        <Routing addToCart={addToCart} />
       </main>
     </div>
   );
