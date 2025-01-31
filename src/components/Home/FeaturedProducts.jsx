b6070d5e01aea8e3be7614eaf0f4b53569cc39e9
rok <rok@gmail.com>
2025-01-31 09:43:14 +0900

    미션 홈화면 대표상품, 바로구매 링크 설정

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./FeaturedProducts.css";
 import ProductCard from "../Products/ProductCard";
+import useData from "../Hook/useData";
+import ProductCardSkeleton from "../Products/ProductCardSkeleton";
 
 const FeaturedProducts = () => {
+  const { data: products, error, isLoading } = useData("products/featured");
+  console.log(products);
+  const skeletons = [1, 2, 3];
   return (
     <section className="featured_products">
       <h2>주요제품</h2>
 
       <div className="align_center featured_products_list">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+
+        {products &&
+          !isLoading &&
+          products.map((p) => <ProductCard key={p._id} product={p} />)}
       </div>
     </section>
   );
